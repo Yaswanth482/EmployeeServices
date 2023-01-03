514d8f80e397add0b3b8a78f41da594bd9e0d152
Yaswanth482 <119789442+Yaswanth482@users.noreply.github.com>
2023-01-03 17:15:50 +0530

    Changing the port number

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,55 @@
 package com.example.demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.entity.Employee;
+import com.example.demo.repository.DeptRepository;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	DeptRepository dept;
+	
 	@Test
-	void contextLoads() {
+	public void testCreate() {
+		Employee e = new Employee();
+		e.setId(1L);
+		e.setTitle("yaswanth");
+		e.setPublished("SC");
+		e.setDescription("LE");
+		dept.save(e);
+		
+		assertNotNull(dept.findById(1L).get());
+		
 	}
-
+	@Test
+	public void testReadAll() {
+//		Employee e = new Employee();
+//		e.setId(1L);
+//		e.setTitle("yaswanth");
+//		e.setPublished("SC");
+//		e.setDescription("LE");
+//		dept.save(e);
+//		
+//		Employee e1 = new Employee();
+//		e1.setId(2L);
+//		e1.setTitle("sumanth");
+//		e1.setPublished("cse");
+//		e1.setDescription("aiml");
+//		dept.save(e1);
+		
+		
+		List<Employee> list = dept.findAll();
+		System.out.println("printing value of list:"+ list.size());
+		assertThat(list).size().isGreaterThan(0);
+		
+	}
 }
